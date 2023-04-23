6c7423fc664f1becee553a2a210283ca97c3169c
anurpeljto <anur.peljto@stu.ibu.edu.ba>
2023-04-23 13:44:01 +0200

    completed task 1

---
+++ task_1/index.php
@@ -0,0 +1,41 @@
+<?php
+
+/*Write your own PHP functions to calculate and display average, lowest and highest temperatures.
+Recorded temperatures:
+ 78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 75, 76, 73, 68, 62, 73, 72, 65, 74, 62, 62, 65, 64, 68,
+  73, 75, 79, 73
+*/
+
+function avg($temperature){
+    $sum = 0;
+     $count = 0;
+    foreach ($temperature as $temp){
+        $sum += $temp;
+        $count+=1;
+    }
+    return sum / count;
+}
+
+// return highest:
+
+function highest($temperature){
+    $max = $temperature[0];
+    foreach($temperature as $temp){
+        if ($temp > $max){
+            $max = $temp;
+        }
+    }
+    return $max;
+}
+
+// return lowest
+
+function lowest($temperature){
+    $min = $temperature[0];
+    foreach ($temperature as $temp){
+        if ($temp < $min){
+            $min = $temp;
+        }
+    }
+    return $min;
+}
