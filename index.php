e4f75d2a5a91af1f363f79dc5ec81720ee5f4b0b
anurpeljto <anur.peljto@stu.ibu.edu.ba>
2023-04-23 20:51:30 +0200

    test

--- index.php
+++ index.php
@@ -1,16 +1,10 @@
 <?php
 
-
 require 'vendor/autoload.php';
 
 // mysql:host=HOSTNAME;dbname=DBNAME','USERNAME','PASSWORD'
 Flight::register('db', 'PDO', array('mysql:host=localhost;dbname=lab3','root','root'));
 
-Flight::route('GET /api/users', function(){
-    $users = Flight::db()->query('SELECT * FROM Users', PDO::FETCH_ASSOC)->fetchAll();
-    var_dump($users);
-    Flight::json($users);
-});
 
 Flight::start();
 
