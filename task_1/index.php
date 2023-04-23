40327f6595b7524107cf19bbc76b62fca260648e
anurpeljto <anur.peljto@stu.ibu.edu.ba>
2023-04-23 13:47:45 +0200

    task1

--- task_1/index.php
+++ task_1/index.php
@@ -1,5 +1,14 @@
 <?php
 
+require 'flight/Flight.php';
+
+Flight::route('/', function(){
+  echo 'hello world!';
+});
+
+Flight::start();
+
+
 /*Write your own PHP functions to calculate and display average, lowest and highest temperatures.
 Recorded temperatures:
  78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 75, 76, 73, 68, 62, 73, 72, 65, 74, 62, 62, 65, 64, 68,
@@ -39,3 +48,5 @@ function lowest($temperature){
     }
     return $min;
 }
+
+?>
