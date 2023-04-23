ef52d0c931c74b4fb8526c45a6e4da7afd90a736
anurpeljto <anur.peljto@stu.ibu.edu.ba>
2023-04-23 17:01:11 +0200

    connected db

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS, PATCH');
+require 'vendor/autoload.php';
 require 'flight/Flight.php';
 
 Flight::route('/', function(){
