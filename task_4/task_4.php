44277956325a17481e3b9b554a4a544290d45765
anurpeljto <anur.peljto@stu.ibu.edu.ba>
2023-04-23 17:13:44 +0200

    .

--- task_4/task_4.php
+++ task_4/task_4.php
@@ -12,9 +12,6 @@ try{
     $conn ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
 
-    $stmt = $conn ->prepare("INSERT INTO Users (firstName, lastName) VALUES ('koo', 'rac'");
-    $result = $stmt -> execute();
-    print_r($result);
 
     $stmt2 = $conn->prepare("SELECT * FROM Users");
     $res2 = $stmt2 -> execute();
