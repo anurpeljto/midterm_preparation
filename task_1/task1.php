9554d7bb1ed9813537d605a1a7dfdce64b13ec94
anurpeljto <anur.peljto@stu.ibu.edu.ba>
2023-04-23 14:07:01 +0200

    task2

--- task_1/task1.php
+++ task_1/task1.php
@@ -41,7 +41,7 @@ function lowest($temperature){
     return $min;
 }
 
-echo 'Testing functions';
+echo 'Testing functions<br>';
 $array = [
     78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 75, 76, 73, 68, 62, 73, 72, 65, 74, 62, 62, 65, 64, 68, 73, 75, 79, 73
 ];
---
+++ task_2/task2.php
@@ -0,0 +1,28 @@
+<?php
+
+function transformArray($array){
+    // array(4) { ["Sarajevo"]=> int(71000) ["Mostar"]=> int(88000) ["Tuzla"]=> int(75000) ["Zenica"]=> int(72000) }
+    // into : array(4) { ["Sarajevo"]=> string(7) "capital" ["Mostar"]=> int(88000) ["Tuzla"]=> int(75000) ["Banja Luka"]=> int(78000) }
+    $array["Sarajevo"] = "capital";
+    $array["Mostar"] = 88000;
+    $array["Banja Luka"] = 78000;
+    unset($array["Zenica"]);
+};
+
+$array = [
+    ["Sarajevo"]=> int(71000),
+    ["Mostar"]=> int(88000),
+    ["Tuzla"]=> int(75000),
+    ["Zenica"]=> int(72000)
+];
+
+echo 'Initial array: <br>';
+foreach($array as $element){
+     echo $element . '<br>';
+    };
+
+echo 'Modified array: <br>';
+transformArray($array);
+foreach($array as $element){
+    echo $element . '<br>';
+}
