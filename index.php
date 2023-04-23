d84fe957b3d65f7e50c5195600712770d2406c43
anurpeljto <anur.peljto@stu.ibu.edu.ba>
2023-04-23 20:40:24 +0200

    asd

--- index.php
+++ index.php
@@ -3,10 +3,22 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS, PATCH');
 require 'vendor/autoload.php';
 require 'flight/Flight.php';
+require_once '/rest/dao/ProjectDao.class.php';
 
 Flight::route('/', function(){
   echo 'hello world!';
 });
+
+$projectDao = new ProjectDao();
+
+$anur = [
+  "firstName" => "Anur",
+  "id" => "1",
+  "lastName" => "Peljto"
+];
+
+$projectDao->addObject($anur);
+$projectDao->getAll();
 
 Flight::start();
 
