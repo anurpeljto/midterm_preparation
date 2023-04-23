757aa4eda59c22487d5ff0a687bb08d8c252c083
anurpeljto <anur.peljto@stu.ibu.edu.ba>
2023-04-23 20:43:22 +0200

    .

--- index.php
+++ index.php
@@ -1,26 +1,19 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS, PATCH');
-require 'vendor/autoload.php';
-require 'flight/Flight.php';
-require_once '/rest/dao/ProjectDao.class.php';
 
-Flight::route('/', function(){
-  $anur = [
-    "firstName" => "Anur",
-    "id" => "1",
-    "lastName" => "Peljto"
-  ];
-  
-  $projectDao->addObject($anur);
-  $projectDao->getAll();
-  echo "Hi";
+
+require 'vendor/autoload.php';
+
+// mysql:host=HOSTNAME;dbname=DBNAME','USERNAME','PASSWORD'
+Flight::register('db', 'PDO', array('mysql:host=localhost;dbname=lab3','root','root'));
+
+Flight::route('GET /api/users', function(){
+    $users = Flight::db()->query('SELECT * FROM Users', PDO::FETCH_ASSOC)->fetchAll();
+    var_dump($users);
+    Flight::json($users);
 });
 
-$projectDao = new ProjectDao();
-
-
-
 Flight::start();
+
+echo "Hello World!";
 
 ?>
